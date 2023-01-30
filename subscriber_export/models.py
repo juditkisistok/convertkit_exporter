05cc968a1b5f88b728b1e6b8ba2a7b2cee2e115f
juditkisistok <judit.kisistok@gmail.com>
2023-01-30 10:31:49 +0100

    adding tag model

--- subscriber_export/models.py
+++ subscriber_export/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    tagid = models.IntegerField(unique=True)
+    retrieved = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
