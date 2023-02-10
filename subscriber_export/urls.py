e7580bdfddbccf6307e002b3dd1f7f076f26788e
juditkisistok <judit.kisistok@gmail.com>
2023-02-10 16:41:13 +0100

    styling tag display

--- subscriber_export/urls.py
+++ subscriber_export/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('tags/', views.display_tags, name='display_tags'),
     path('', views.display_tags, name='display_tags'),
     path('download/<int:tag>_<str:name>', views.download_tag_subs, name='download'),
+    path('download-all/', views.download_all_subs, name='download-all'),
     path('api/', views.inputAPI, name='inputAPI'),
     path('update-api/<str:email>', views.updateAPI, name='updateAPI'),
     path('delete-api/<str:email>', views.deleteAPI, name='deleteAPI'),
