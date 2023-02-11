b8053a752e2e4a08e50c659f9c7cda3a3c2af25c
juditkisistok <judit.kisistok@gmail.com>
2023-02-11 22:36:39 +0100

    moving to postgres

--- convertkit_exporter/settings.py
+++ convertkit_exporter/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'convertkit_exporter.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'convertkit_exporter',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRES_PW'],
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
 # Password validation
