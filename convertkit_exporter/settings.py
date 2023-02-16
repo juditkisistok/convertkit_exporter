d1b1b200f53429c4ef717435810da694da42e4ea
juditkisistok <judit.kisistok@gmail.com>
2023-02-16 20:01:46 +0100

    fixing deployment warnings

--- convertkit_exporter/settings.py
+++ convertkit_exporter/settings.py
@@ -20,12 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 FIELD_ENCRYPTION_KEY = os.environ.get('FIELD_ENCRYPTION_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False#os.environ.get('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -85,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'convertkit_exporter',
-        'USER': 'juditkisistok',
+        'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PW'],
         'HOST': os.environ['AWS_URL'],
         'PORT': '5432'
@@ -133,6 +131,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+#SECURE_HSTS_SECONDS = 0 #31536000
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SECURE_HSTS_PRELOAD = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
